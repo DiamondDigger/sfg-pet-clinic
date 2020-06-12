d2324dfe08100a736d9f9aa4844947df13ee853a
naglec13 <naglec13@gmail.com>
2020-06-12 16:00:22 +0300

    refactoring to multi-module maven project. Closes #1

--- pet-clinic-data/src/main/java/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/model/Pet.java
@@ -1,4 +1,4 @@
-package com.springframework.sfgpetclinic.model;
+package sfgpetclinic.model;
 
 import java.time.LocalDate;
 
--- pet-clinic-web/src/main/java/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/SfgPetClinicApplication.java
@@ -1,5 +1,3 @@
-package com.springframework.sfgpetclinic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
