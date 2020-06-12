746091822a65de1a9d557f8f39f0d5215737b649
naglec13 <naglec13@gmail.com>
2020-06-12 12:24:13 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/com/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package com.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private Owner owner;
+    private PetType petType;
+    private LocalDate birthDate;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public LocalDate getbirthDate() {
+        return birthDate;
+    }
+
+    public void setbirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
